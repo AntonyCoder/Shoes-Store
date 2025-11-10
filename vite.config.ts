8efe2dd5e3c5bebdcc7f28b5667a79a06814a71d
Anton Lysenko <antonlysenko161@yandex.ru>
2025-11-10 11:32:10 +0300

    Скорректировал сборку проекта

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +9,16 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  }
 });
